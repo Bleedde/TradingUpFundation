535c7eb4ec0b95ce9ce2b5be8ec9b805fc4558a3
bleed <ttreyygh@gmail.com>
2023-08-11 09:10:31 -0500

    NavScrollAnimation

--- Trading-Frontend/src/app/module/home/home-page/components/navbar/navbar.component.ts
+++ Trading-Frontend/src/app/module/home/home-page/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 import { IMAGEN_LOGO } from 'src/app/shared/constants';
 
 @Component({
@@ -8,4 +8,24 @@ import { IMAGEN_LOGO } from 'src/app/shared/constants';
 })
 export class NavbarComponent {
   imagen_logo = IMAGEN_LOGO;
+
+  scrolled = false;
+  private scrollTimeout: any;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = true;
+    clearTimeout(this.scrollTimeout);
+
+    this.scrollTimeout = setTimeout(() => {
+      this.scrolled = false;
+    }, 200); // Cambia el valor según la cantidad de tiempo de inactividad deseada (en milisegundos)
+  }
+
+  scroll(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if(element){
+      element.scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+  }
 }
