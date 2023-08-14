e4e66525f837b59cbeb6a9b513707e46fab24b55
Brahian Barrios <ttreyygh@gmail.com>
2023-08-13 21:06:28 -0500

    footer

--- Trading-Frontend/src/app/module/home/home-page/components/footer/footer.component.ts
+++ Trading-Frontend/src/app/module/home/home-page/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMAGEN_LOGO } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  logo = IMAGEN_LOGO;
 }
