0559519ad164e5ef490e5fcd2a984b0ce5a6e65b
bleed <ttreyygh@gmail.com>
2023-08-17 11:05:57 -0500

    login-update

--- Trading-Frontend/src/app/module/login/login-page/components/login/login.component.ts
+++ Trading-Frontend/src/app/module/login/login-page/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IMAGEN_LOGO, LOGIN } from 'src/app/shared/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,19 @@ import { IMAGEN_LOGO, LOGIN } from 'src/app/shared/constants';
 export class LoginComponent {
   loginImage = LOGIN;
   logo = IMAGEN_LOGO;
-  imagenUrl = '/src/assets/img/loginImage.jpg';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  scrollToComponent(): void {
+    this.router.navigate(['/home-page'], { fragment: 'contact' });
+    setTimeout(() => {
+      const element = document.getElementById('contact');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 2000); // Espera 500 ms para dar tiempo a que Angular realice la navegación
+  }
 }
