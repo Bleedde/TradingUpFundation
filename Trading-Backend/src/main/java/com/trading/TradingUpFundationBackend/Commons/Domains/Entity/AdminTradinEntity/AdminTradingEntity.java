bfcad8959d981d7c57097bc24ed9118bd4546a5f
Juan Ospina <125884403+JuanDavidOspinaDelgadillo@users.noreply.github.com>
2023-09-07 20:31:26 -0500

    TradingUp-043 CrearConverterEntidadDTO - Springboot V2

--- Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/Commons/Domains/Entity/AdminTradinEntity/AdminTradingEntity.java
+++ Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/Commons/Domains/Entity/AdminTradinEntity/AdminTradingEntity.java
@@ -1,31 +1,34 @@
 package com.trading.TradingUpFundationBackend.Commons.Domains.Entity.AdminTradinEntity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.trading.TradingUpFundationBackend.Commons.Domains.Entity.UserTradingEntity.UserTradingEntity;
-import jakarta.persistence.*;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonManagedReference;//Package to deny the recursion of the side "One"
+import com.trading.TradingUpFundationBackend.Commons.Domains.Entity.UserTradingEntity.UserTradingEntity;//Import the entity "User" to represent the relation between these entities
+import jakarta.persistence.*;//Package to add the persistence to this entity
+import lombok.Data;//Package of lombok to add the normal methods that an entity has
 
-import java.util.List;
+import java.util.List;//Package to manage a dynamic list
 
-@Entity
-@Data
-@Table(name = "admin_trading")
+@Entity//Annotation to represent this class like an entity of a database for spring
+@Data//Annotation to represent normal methods of a normal class
+@Table(name = "admin_trading")//Annotation to represent this entity like a table in a database represented with a name
+/**
+ * Class to represent the entity "admin_trading" in de modeling "TradingUpFoundation"
+ */
 public class AdminTradingEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "admin_id")
+    @Id//Annotation to represent this attribute like an ID who represent this Entity
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Annotation who represent the way of how the id is going to work in each registration
+    @Column(name = "admin_id")//Annotation to represent this attribute like a column with a name in the table
     private Integer id;
 
-    @Column(name = "admin_name", length = 255, nullable = false)
+    @Column(name = "admin_name", length = 255, nullable = false)//Annotation to represent this attribute like a column with a name in the table, the length has to be 255 and it cant be null
     private String name;
 
-    @Column(name = "admin_email", length = 255, nullable = false)
+    @Column(name = "admin_email", length = 255, nullable = false)//Annotation to represent this attribute like a column with a name in the table, the length has to be 255 and it cant be null
     private String email;
 
-    @Column(name = "admin_password", length = 255, nullable = false)
+    @Column(name = "admin_password", length = 255, nullable = false)//Annotation to represent this attribute like a column with a name in the table, the length has to be 255 and it cant be null
     private String password;
 
-    @OneToMany(mappedBy = "adminTradingEntityRelation", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonManagedReference
+    @OneToMany(mappedBy = "adminTradingEntityRelation", cascade = CascadeType.ALL, orphanRemoval = true)//Annotation to represent a relation "One" to "Many" where it is mapped the attribute in the another entity who represent the side "Many"
+    @JsonManagedReference//Annotation to do only a query to the entity who represent the side "Many" in the relation
     private List<UserTradingEntity> userTradingEntityRelation;
 }
--- Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/Commons/Domains/Entity/ClassTradingEntity/ClassTradingEntity.java
+++ Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/Commons/Domains/Entity/ClassTradingEntity/ClassTradingEntity.java
@@ -1,30 +1,33 @@
 package com.trading.TradingUpFundationBackend.Commons.Domains.Entity.ClassTradingEntity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.trading.TradingUpFundationBackend.Commons.Domains.Entity.LevelTradingEntity.LevelTradingEntity;
-import jakarta.persistence.*;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonBackReference;//Package to deny the recursion of the side "One"
+import com.trading.TradingUpFundationBackend.Commons.Domains.Entity.LevelTradingEntity.LevelTradingEntity;//Import the entity "Level" to represent the relation between these entities
+import jakarta.persistence.*;//Package to add the persistence to this entity
+import lombok.Data;//Package of lombok to add the normal methods that an entity has
 
-@Entity
-@Table(name = "class_trading")
-@Data
+@Entity//Annotation to represent this class like an entity of a database for spring
+@Table(name = "class_trading")//Annotation to represent this entity like a table in a database represented with a name
+@Data//Annotation to represent normal methods of a normal class
+/**
+ * Class to represent the entity "class_trading" in the modeling "TradingUpFoundation"
+ */
 public class ClassTradingEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "class_id")
+    @Id//Annotation to represent this attribute like an ID who represent this Entity
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Annotation who represent the way of how the id is going to work in each registration
+    @Column(name = "class_id")//Annotation to represent this attribute like a column with a name in the table
     private Integer id;
 
-    @Column(name = "class_title", length = 255, nullable = false)
+    @Column(name = "class_title", length = 255, nullable = false)//Annotation to represent this attribute like a column with a name in the table, the length has to be 255 and it cant be null
     private String name;
 
-    @Column(name = "class_subtitle", length = 255, nullable = false)
+    @Column(name = "class_subtitle", length = 255, nullable = false)//Annotation to represent this attribute like a column with a name in the table, the length has to be 255 and it cant be null
     private String subtitle;
 
-    @Column(name = "class_description", length = 255, nullable = false)
+    @Column(name = "class_description", length = 255, nullable = false)//Annotation to represent this attribute like a column with a name in the table, the length has to be 255 and it cant be null
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "level_id")
-    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)//Annotation to represent the relation "Many" to "One"
+    @JoinColumn(name = "level_id")//Annotation to represent to what column is gonna represent the ID who is the foreign key in this side of this relation
+    @JsonBackReference//Annotation to do only a query to the entity who represent the side "One" in the relation
     private LevelTradingEntity levelTradingEntityRelation;
 }
