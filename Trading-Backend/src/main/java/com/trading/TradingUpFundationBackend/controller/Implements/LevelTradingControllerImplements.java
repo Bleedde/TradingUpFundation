4b1d121a490702c76e851798e5e3d35f8abf7e54
JuanDavidOspinDelgadillo <ttreyygh@gmail.com>
2023-10-04 11:37:59 -0500

    TradingUp-036 CrearControladorDeEntidadLevel_Trading - Springboot

--- Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/controller/Implements/LevelTradingControllerImplements.java
+++ Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/controller/Implements/LevelTradingControllerImplements.java
@@ -1,4 +1,45 @@
 package com.trading.TradingUpFundationBackend.controller.Implements;
 
-public class LevelTradingControllerImplements {
+import org.springframework.http.ResponseEntity;
+
+import com.trading.TradingUpFundationBackend.commons.domains.ObjectResponse;
+import com.trading.TradingUpFundationBackend.commons.domains.DTO.LevelTradingDTO;
+import com.trading.TradingUpFundationBackend.controller.ILevelTradingController;
+import com.trading.TradingUpFundationBackend.service.IMPL.LevelTradingService;
+
+public class LevelTradingControllerImplements implements ILevelTradingController{
+
+    private final LevelTradingService service;
+    public LevelTradingControllerImplements(LevelTradingService service){
+        this.service = service;
+    }
+
+    @Override
+    public ResponseEntity<ObjectResponse> createLevelTrading(LevelTradingDTO levelTradingDTO) {
+        return this.service.createLevelTrading(levelTradingDTO);
+    }
+
+    @Override
+    public ResponseEntity<ObjectResponse> readLevelTrading(LevelTradingDTO levelTradingDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readLevelTrading'");
+    }
+
+    @Override
+    public ResponseEntity<ObjectResponse> readLevelsTrading() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readLevelsTrading'");
+    }
+
+    @Override
+    public ResponseEntity<ObjectResponse> updateLevelTrading(LevelTradingDTO levelTradingDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateLevelTrading'");
+    }
+
+    @Override
+    public ResponseEntity<ObjectResponse> deleteLevelTrading(LevelTradingDTO levelTradingDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteLevelTrading'");
+    }
 }
