5f85f171cc305d4f4300538177bb5dda0443f2be
Brahian Barrios <ttreyygh@gmail.com>
2023-10-10 19:43:44 -0500

    arregloLogin

--- Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/commons/constant/URLs/IExerciseTradingEndPoints.java
+++ Trading-Backend/src/main/java/com/trading/TradingUpFundationBackend/commons/constant/URLs/IExerciseTradingEndPoints.java
@@ -8,5 +8,5 @@ public interface IExerciseTradingEndPoints {
     String URL_EXERCISE_DELETE = "/exercise_delete";//Delete a exercise area
     String URL_EXERCISE_UPDATE = "/exercise_update";//Update a exercise area
     String URL_EXERCISE_READ = "/exercise_read";//Search a exercise area
-    String URL_EXERCISES_READ = "/exercise_read";//Search all the exercises area
+    String URL_EXERCISES_READ = "/exercises_read";//Search all the exercises area
 }
